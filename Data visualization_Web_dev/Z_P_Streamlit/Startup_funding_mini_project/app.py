aa6baf1cb349582c2c2331983b9a627a60da603b
manishkumarmj <manishtheprogrammer@gmail.com>
2026-07-16 22:27:16 +0530

    investor analysis-- uptil year on year growth

--- Data visualization_Web_dev/Z_P_Streamlit/Startup_funding_mini_project/app.py
+++ Data visualization_Web_dev/Z_P_Streamlit/Startup_funding_mini_project/app.py
@@ -1,35 +1,73 @@
 import streamlit as st
-import pandas as pd 
-import matplotlib.pyplot as plt 
+import pandas as pd  
+import matplotlib.pyplot as plt
+import numpy as np
+
+st.set_page_config(layout="wide",page_title="Startup Analysis")  
 
 
 # dataframe 
 df = pd.read_csv(r'A:\AIML_DEV_FOLDER\Data visualization_Web_dev\Z_P_Streamlit\Startup_funding_mini_project\startup_clean.csv')
-
+df.loc[df['amount'] == 0, 'amount'] = np.random.randint(10, 101, size=(df['amount'] == 0).sum())
 
 
 
 def load_investor_details(investor):
     st.title(investor)
     # last 5 
-    last_5 = df[df["investors"].str.contains('investor')].head()[["date","starup","Vertical","Round","amount"]]
+    last_5 =  df[df['investors'].str.contains(investor)].head()[["date","starup","Vertical","Round","amount"]]
     st.subheader("most recent investment ")
     st.dataframe(last_5)
-    # bigest investment 
-    biggest=df[df['investors'].str.contains('investor')].groupby("starup")['amount'].sum().sort_values(ascending=False).head(5)
+
+
+
+    #  bigest investment 
+    biggest=df[df['investors'].str.contains(investor)].groupby("starup")['amount'].sum().sort_values(ascending=False).head()
     st.subheader("Big boie")
-    st.dataframe(biggest)
     fig,ax = plt.subplots()
     ax.bar(biggest.index,biggest.values)
     st.pyplot(fig)
 
 
-    fig, ax = plt.subplots()
-    ax.pie(biggest.values, labels=biggest.index, autopct='%1.1f%%')
-    ax.axis('equal')  # Equal aspect ratio ensures pie is circular
-    st.pyplot(fig)
 
-# filling NA  value in Investors Name
+    # vertical round
+    vertical_series = df[df['investors'].str.contains(investor)].groupby('Vertical')["amount"].sum()
+    st.subheader('Sectors invested in vertical round')
+    fig1, ax1 = plt.subplots()
+    ax1.pie(vertical_series.values, labels=vertical_series.index, autopct='%1.1f%%')
+    st.pyplot(fig1)
+    
+    
+    # city  
+
+
+    city_series = df[df['investors'].str.contains(investor, case=False, na=False)].groupby('city')["amount"].sum().sort_values(ascending=False).head(5)
+    st.subheader('city inviested in ')
+    fig2, ax2 = plt.subplots()
+    ax2.pie(city_series.values, labels=city_series.index, autopct='%1.1f%%')
+    ax2.axis('equal')
+    st.pyplot(fig2)
+
+
+    # stage_series
+    stage_series = df[df['investors'].str.contains(investor, case=False, na=False)].groupby('Round')["amount"].sum().sort_values(ascending=False).head(3)
+
+    st.subheader('Stage ')
+    fig3, ax3 = plt.subplots()
+    ax3.pie(stage_series.values, labels=stage_series.index, autopct='%1.1f%%')
+    ax3.axis('equal')
+    st.pyplot(fig3)
+
+
+    # year on year
+    df['date'] = pd.to_datetime(df['date'], errors='coerce')
+    df['year']=df["date"].dt.year 
+    year_on_year=df[df['investors'].str.contains(investor)].groupby('year')["amount"].sum()
+    st.subheader("Year on year growth")
+    fig4,ax4 = plt.subplots()
+    ax4.plot(year_on_year.index,year_on_year.values)
+    st.pyplot(fig4)
+
 
 
 
@@ -48,9 +86,7 @@ elif option == "Start-up":
 else:
 
     st.title("Investor Analysis")
-
     selected_investor=st.sidebar.selectbox("Select Star-UP", sorted(set(df['investors'].str.split(',').sum())))
-
     btn2 = st.sidebar.button("find Investor  Details")
     if btn2: 
         load_investor_details(selected_investor)
